f7cf625b9a9d13ebe01ac035e0e35d10ad8cbadb
Serena <serena.toqeer@gmail.com>
2026-03-24 22:18:36 -0400

    add dockerfile for huggingface

--- app.py
+++ app.py
@@ -323,5 +323,5 @@ def index():
     return render_template("index.html", results=results)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 7860))
     app.run(host="0.0.0.0", port=port)
